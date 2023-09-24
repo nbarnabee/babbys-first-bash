d122b5f634c29b0e20b4a41d7a57453c81b82473
nbarnabee <nicole.barnabee@gmail.com>
2023-09-24 16:09:46 +0200

    Commit practice scripts

---
+++ parameters.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Run this script with bash parameters.sh <your name>
+echo Hello, $1, thank you for using $0
---
+++ show_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo The current user is $USER .
+echo Your home directory is $HOME .
